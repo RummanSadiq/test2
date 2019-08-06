b80c61ec84f14ecdb9958671a015c1b9fa0ca2de
Rumman Sadiq <chrumman@gmail.com>
2019-08-06 15:04:40 +0500

    testing2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,9 @@ class CommentsController < ApplicationController
 	def create
 	    @article = Article.find(params[:article_id])
 	    @comment = @article.comments.create(comment_params)
-	    redirect_to article_path(@article)
+		redirect_to article_path(@article)
+		
+		# Rumman Sadiq New Test Case
   	end
  
    def destroy
